8b39800845841abd0780a615fc4f487d6af07c0f
EricCahyawan <137325542+EricCahyawan@users.noreply.github.com>
2024-10-29 01:44:27 +0700

    Commit satu

--- app/src/main/java/eric/latihanfragmentrumit/fSatu.kt
+++ app/src/main/java/eric/latihanfragmentrumit/fSatu.kt
@@ -1,14 +1,20 @@
 package eric.latihanfragmentrumit
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
+import org.w3c.dom.Text
 
 // TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
@@ -18,9 +24,9 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class fSatu : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var score: Int = 50
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -34,21 +40,170 @@ class fSatu : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_f_satu, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val firstNumber = 1
+        val lastNumber = firstNumber + 4
+        val numbers = Array(10) { 0 }
+        var firstCardOpen = false
+        var secondCardOpen = false
+        var firstCardValue: Int? = null
+        var secondCardValue: Int? = null
+
+        var cardOpen = arrayOf(false, false, false, false, false, false, false, false, false, false)
+
+        var index = 0
+        for (i in firstNumber..lastNumber) {
+            for (j in 1..2) {
+                numbers[index] = i
+                index++
+            }
+        }
+
+        numbers.shuffle()
+
+        val _score = view.findViewById<TextView>(R.id.score)
+        _score.text = "Score: $score"
+
+        val _giveUp = view.findViewById<TextView>(R.id.giveUp) // Ubah ke Button jika _giveUp adalah Button
+
+        val textViews = arrayOf(
+            view.findViewById<TextView>(R.id.tv1),
+            view.findViewById<TextView>(R.id.tv2),
+            view.findViewById<TextView>(R.id.tv3),
+            view.findViewById<TextView>(R.id.tv4),
+            view.findViewById<TextView>(R.id.tv5),
+            view.findViewById<TextView>(R.id.tv6),
+            view.findViewById<TextView>(R.id.tv7),
+            view.findViewById<TextView>(R.id.tv8),
+            view.findViewById<TextView>(R.id.tv9),
+            view.findViewById<TextView>(R.id.tv10)
+        )
+
+        for (i in textViews.indices) {
+            textViews[i].text = numbers[i].toString()
+        }
+
+        val cardViews = arrayOf(
+            view.findViewById<CardView>(R.id.card1),
+            view.findViewById<CardView>(R.id.card2),
+            view.findViewById<CardView>(R.id.card3),
+            view.findViewById<CardView>(R.id.card4),
+            view.findViewById<CardView>(R.id.card5),
+            view.findViewById<CardView>(R.id.card6),
+            view.findViewById<CardView>(R.id.card7),
+            view.findViewById<CardView>(R.id.card8),
+            view.findViewById<CardView>(R.id.card9),
+            view.findViewById<CardView>(R.id.card10)
+        )
+
+        fun resetCards() {
+            Handler(Looper.getMainLooper()).postDelayed({
+                for (i in cardOpen.indices) {
+                    if (cardOpen[i]) {
+                        textViews[i].visibility = View.INVISIBLE
+                        cardViews[i].setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.grey))
+                        cardOpen[i] = false
+                    }
+                }
+                firstCardOpen = false
+                secondCardOpen = false
+                firstCardValue = null
+                secondCardValue = null
+            }, 1000)
+        }
+
+        fun checkMatch() {
+            if (firstCardValue != null && secondCardValue != null) {
+                if (firstCardValue == secondCardValue) {
+                    score += 10
+                    _score.text = "Score: $score"
+
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        for (i in cardOpen.indices) {
+                            if (cardOpen[i]) {
+                                textViews[i].visibility = View.INVISIBLE
+                                cardViews[i].visibility = View.INVISIBLE
+                            }
+                        }
+                    }, 1000)
+
+                    if (cardOpen.all { !it }) {
+                        // Pindah ke fragment fDua saat semua kartu hilang
+                        navigateToFragmentDua()
+                    }
+                } else {
+                    score -= 5
+                    _score.text = "Score: $score"
+                    resetCards()
+                }
+                firstCardOpen = false
+                secondCardOpen = false
+                firstCardValue = null
+                secondCardValue = null
+            }
+        }
+
+        fun handleCardClick(cardIndex: Int, textView: TextView, card: CardView) {
+            if (!firstCardOpen && !secondCardOpen) {
+                textView.visibility = View.VISIBLE
+                card.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+                cardOpen[cardIndex] = true
+                firstCardOpen = true
+                firstCardValue = numbers[cardIndex]
+            } else if (firstCardOpen && !secondCardOpen) {
+                if (cardOpen[cardIndex]) {
+                    textView.visibility = View.INVISIBLE
+                    card.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.grey))
+                    cardOpen[cardIndex] = false
+                    firstCardOpen = false
+                } else {
+                    textView.visibility = View.VISIBLE
+                    card.setBackgroundColor(ContextCompat.getColor(requireContext(), R.color.white))
+                    cardOpen[cardIndex] = true
+                    secondCardOpen = true
+                    secondCardValue = numbers[cardIndex]
+                    checkMatch()
+                }
+            }
+        }
+
+        for (i in cardViews.indices) {
+            cardViews[i].setOnClickListener {
+                handleCardClick(i, textViews[i], cardViews[i])
+            }
+        }
+
+        // Set OnClickListener untuk tombol give up
+        _giveUp.setOnClickListener {
+            navigateToFragmentDua()
+        }
+    }
+
+    // Method untuk pindah ke fragment fDua
+    private fun navigateToFragmentDua() {
+        val bundle = Bundle()
+        bundle.putInt("score", score)
+        val mfDua = fDua()
+        mfDua.arguments = bundle
+        val mFragmentManager = parentFragmentManager
+        mFragmentManager.beginTransaction().apply {
+            replace(R.id.frameLayout, mfDua, fDua::class.java.simpleName)
+            addToBackStack(null)
+            commit()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        score = 50
+        view?.findViewById<TextView>(R.id.score)?.text = "Score: $score"
+    }
 
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment fSatu.
-         */
-        // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
             fSatu().apply {
@@ -59,3 +214,4 @@ class fSatu : Fragment() {
             }
     }
 }
+
